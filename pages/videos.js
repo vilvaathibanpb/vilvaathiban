f6fa9435c03e84d832bcdab25f88919bd8c32497
Vilva Athiban <vilvaathiban@omios-MacBook-Pro-8.local>
2020-12-05 01:09:13 +0530

    add meta tags

--- pages/videos.js
+++ pages/videos.js
@@ -16,6 +16,26 @@ export default function VideosPage() {
         </title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#3AAFA8" />
+        <meta property="og:site_name" content="vilvaathiban.com" />
+        <meta property="og:site" content="vilvaathiban.com" />
+        <meta
+          property="og:title"
+          content="Vilva Athiban - The JS Jockey | JavaScript Expert & Mentor"
+        />
+        <meta
+          property="og:description"
+          content="Vilva Athiban is a Web expert who specialize in JavsScript. He is active Open source developer and loves sharing his knowledge through talks, workshops, blogs, videos etc"
+        />
+        <meta property="og:type" content="portfolio" />
+
+        <meta
+          name="description"
+          content="Vilva Athiban is a Web expert who specialize in JavsScript. He is active Open source developer and loves sharing his knowledge through talks, workshops, blogs, videos etc"
+        />
+        <meta
+          name="keywords"
+          content="react, javascript, training, tutorial, learn, mentor, instructor, coaching, startup, javascript, graphql, apollo, webpack, reactjs, angularjs, nodejs"
+        />
       </Head>
       <Header />
       <ItemsContainer>
