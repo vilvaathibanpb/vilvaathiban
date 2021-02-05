2ce07c8833530a96e0111ed35bc6e951b63358f9
Vilva Athiban P B <vilvaathiban@gmail.com>
2021-02-05 03:32:22 +0100

    Update workshop.js

--- data/workshop.js
+++ data/workshop.js
@@ -20,16 +20,6 @@ export const workshop = [
     thumbnail: "https://miro.medium.com/max/1540/1*ckWxE4b3zxi1bgZoJFFxOw.jpeg"
   },
   {
-    order: 1,
-    title: "Web Performance - Complete course",
-    date: "06 Mar, 2021",
-    desc: "An advanced practical workshop to understand and improve Web performance",
-    url: "",
-    knowMore: "https://buytickets.at/vilvaathibanpb/482444/r/website",
-    buy: "",
-    thumbnail: "https://images.unsplash.com/photo-1507235071172-438ca6950a8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-  },
-  {
     order: 7,
     title: "Technical SEO for Developers",
     date: "Coming soon",
@@ -68,5 +58,15 @@ export const workshop = [
     knowMore: "",
     buy: "",
     thumbnail: "https://miro.medium.com/max/4260/1*SVyedUtyXmpDcJaXuiZNRg.png"
+  },
+  {
+    order: 1,
+    title: "Web Performance - Complete course",
+    date: "06 Mar, 2021",
+    desc: "An advanced practical workshop to understand and improve Web performance",
+    url: "",
+    knowMore: "https://buytickets.at/vilvaathibanpb/482444/r/website",
+    buy: "",
+    thumbnail: "https://images.unsplash.com/photo-1507235071172-438ca6950a8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzdHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
   },
 ];
